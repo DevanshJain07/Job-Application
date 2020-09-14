fce16941e0def8a83abf6f5bdb2b76981898d4a6
devansh071999 <devanshsampatjain@gmail.com>
2020-09-15 02:24:46 +0530

    job title available

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import FetchJobs from './FetchJobs'
 import {Container} from 'react-bootstrap'
+import Job from './Job'
 function App() {
+  const [params,setParams]=useState({})
+  const [page,setPage]=useState(1)
   const {jobs,loading,error}=FetchJobs()
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing the page.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job=>{
+        return <Job key={job.id} job={job}/>
+      })}
     </Container>
   );
 }
