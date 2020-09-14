e53e1254a0fad55bcd4a6a868e77e242af42b302
devansh071999 <devanshsampatjain@gmail.com>
2020-09-15 03:04:19 +0530

    button working

--- src/Job.js
+++ src/Job.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import {Card, Badge,Button} from 'react-bootstrap'
+import React,{useState} from 'react'
+import {Card, Badge,Button,Collapse} from 'react-bootstrap'
 import ReactMarkdown from 'react-markdown'
 
 export default function Job({job}) {
+    const [open,setOpen]=useState(false)
     return (
         <Card>
             <Card.Body>
@@ -25,11 +26,14 @@ export default function Job({job}) {
                 alt={job.company} src={job.company_logo}/>
                 </div>
                 <Card.Text>
-                    <Button variant="primary">View Details</Button>
+                    <Button variant="primary"
+                    onClick={()=>setOpen(prevOpen=>!prevOpen)}>View Details</Button>
                 </Card.Text>
+                <Collapse in={open}>
                 <div className="mt-4">
                     <ReactMarkdown source={job.description}/>
                 </div>
+                </Collapse>
             </Card.Body>
         </Card>
     )
